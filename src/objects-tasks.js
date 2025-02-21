a26dcc95dfe435d5a53925bbd220d15d5d0389c3
ill_Nino <ill_Nino@bk.ru>
2025-02-21 08:40:00 +0300

    feat: add solution part2

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -147,8 +147,14 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const arr = [];
+  Object.entries(lettersObject).forEach(([key, value]) => {
+    value.forEach((index) => {
+      arr[index] = key;
+    });
+  });
+  return arr.join('');
 }
 
 /**
@@ -165,8 +171,15 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let result = true;
+  queue.reduce((sum, cur) => {
+    if (cur - 25 > sum) {
+      result = false;
+    }
+    return sum + cur;
+  }, 0);
+  return result;
 }
 
 /**
